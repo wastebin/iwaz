b33941150d36bf9fc5cb2fdc93e189f8c1ce6cf1
Heather Felde <hfelde@protonmail.com>
2019-07-24 05:05:54 -0500

    Add basic read loop

--- src/main.c
+++ src/main.c
@@ -1,3 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+#include <histedit.h>
+
 int main(int argc, char **argv) {
+	puts("Īwaz v0.1.0");
+	puts("Press ^C to exit. Please don’t press ^D. It makes editline segfault.");
+
+	while (1) {
+		char *inp = readline("% ");
+
+		add_history(inp);
+
+		free(inp);
+	}
+
 	return 0;
 }
